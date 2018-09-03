4384fe63566bcd4c3facfbd7f1bb23066184b3a8
Filip Ditrich <ditrich@visionarygraphics.cz>
2018-09-03 22:21:55 +0200

    NS-U01 mongoose helper now returns a promise

--- src/helpers/mongoose.helper.js
+++ src/helpers/mongoose.helper.js
@@ -1,13 +1,16 @@
-module.exports = function (mongoose, serviceConfig) {
+exports.connect = function (mongoose, serviceConfig) {
 
-    mongoose.connect(connectionURI(serviceConfig), { useNewUrlParser: true })
-        .then(() => {
-            console.log(`✅ Connection to ${serviceConfig.db.name} database established.`);
-        })
-        .catch(error => {
-            console.error('❌ %s \n %s', `Connection to ${serviceConfig.db.name} database failed.`, error.message);
-            process.exit(1);
-        });
+    return new Promise((resolve, reject) => {
+        mongoose.connect(connectionURI(serviceConfig), { useNewUrlParser: true })
+            .then(() => {
+                console.log(`✅ Connection to ${serviceConfig.db.name} database established.`);
+                resolve();
+            })
+            .catch(error => {
+                console.error('❌ %s \n %s', `Connection to ${serviceConfig.db.name} database failed.`, error.message);
+                reject(error.message);
+            });
+    });
 
 };
 
