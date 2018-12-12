2e199e05b592451a8039e3b1d4f2c73dc67e4403
Filip Ditrich <ditrich@visionarygraphics.cz>
2018-12-12 23:52:35 +0100

    ValidatorHelper fix

--- src/helpers/validator.helper.js
+++ src/helpers/validator.helper.js
@@ -1,5 +1,6 @@
 const schemaFields = require('../assets/schema-fields.asset');
 const codeGenerator = require('./generators/generic-code.generator');
+const moment = require('moment');
 
 /**
  * @description: Validates an inputted email
@@ -68,7 +69,10 @@ exports.inputValidator = class {
      * @param val
      * @return {*}
      */
-    static isDate(val) { return moment(val).isValid(); }
+    static isDate(val) {
+        const d = new Date(val);
+        return d instanceof Date && !isNaN(d.getTime()) && moment(d).isValid();
+    }
 
     /**
      * @description If value's length is larger than <min>
@@ -76,7 +80,7 @@ exports.inputValidator = class {
      * @param min
      * @return {boolean}
      */
-    static min(val, min) { return val.length >= min; }
+    static min(val, min) { return this.isNumber(val) ? val >= min : val.length >= min; }
 
     /**
      * @description If value's length is not larger than <max>
@@ -84,7 +88,7 @@ exports.inputValidator = class {
      * @param max
      * @return {boolean}
      */
-    static max(val, max) { return val.length <= max; }
+    static max(val, max) { return this.isNumber(val) ? val <= max : val.length <= max; }
 
     /**
      * @description If value matches the <regExp>
@@ -94,48 +98,69 @@ exports.inputValidator = class {
      */
     static regExp(val, regExp) { return new RegExp(regExp).test(val); }
 
-    validate(input, validators) {
+    /**
+     * @description Validator
+     * @param input
+     * @param validators
+     * @return {*}
+     */
+    static validate(input, validators) {
 
-        const deep_value = function(obj, path) {
-            let val = false;
-            for (let i = 0, _path = path.split('.'), len = _path.length; i < len; i++) {
-                val = obj[_path[i]];
-            }
-            return val || '';
+        // access the value from the <path> (nested also)
+        const deep_value = function(theObject, path, separator = '.') {
+            try {
+                separator = separator || '.';
+
+                return path.replace('[', separator)
+                    .replace(']','')
+                    .split(separator)
+                    .reduce((obj, property) => obj[property], theObject);
+
+            } catch (err) { return undefined; }
         };
 
         // run validators
         return validators.map(validator => {
-            const value = deep_value(input, validator.field);
             const rules = Object.keys(validator.rules)
                 .map(key => { return { name: key, value: validator.rules[key] } })
                 .filter(x => x.value);
 
+            // get the errors (if any)
             const errors = rules.map(rule => {
+                const value = deep_value(input, validator.field);
+
+                if (!value && rules.findIndex(x => x.name === 'required') >= 0) {
+                    // does not exist but is required
+                    return 'required';
+                } else if (!value && rules.findIndex(x => x.name === 'required') === -1) {
+                    // does not exist but is not required
+                    return false;
+                }
+
                 switch (rule.name) {
-                    case 'required': return inputValidator.isPresent(value) ? false : rule.name;
-                    case 'objectId': return inputValidator.isObjectId(value) ? false : rule.name;
-                    case 'date': return inputValidator.isDate(value) ? false : rule.name;
-                    case 'number': return inputValidator.isNumber(value) ? false : rule.name;
-                    case 'boolean': return inputValidator.isBoolean(value) ? false : rule.name;
-                    case 'min': return inputValidator.min(value, rule.value) ? false : rule.name;
-                    case 'max': return inputValidator.max(value, rule.value) ? false : rule.name;
-                    case 'regExp': return inputValidator.regExp(value, rule.value) ? false : rule.name;
-                    case 'lowercase': return inputValidator.isLowerCase(value) ? false : rule.name;
-                    case 'uppercase': return inputValidator.isUpperCase(value) ? false : rule.name;
+                    case 'required': return this.isPresent(value) ? false : rule.name;
+                    case 'objectId': return this.isObjectId(value) ? false : rule.name;
+                    case 'date': return this.isDate(value) ? false : rule.name;
+                    case 'number': return this.isNumber(value) ? false : rule.name;
+                    case 'boolean': return this.isBoolean(value) ? false : rule.name;
+                    case 'min': return this.min(value, rule.value) ? false : rule.name;
+                    case 'max': return this.max(value, rule.value) ? false : rule.name;
+                    case 'regExp': return this.regExp(value, rule.value) ? false : rule.name;
+                    case 'lowercase': return this.isLowerCase(value) ? false : rule.name;
+                    case 'uppercase': return this.isUpperCase(value) ? false : rule.name;
                     default: return false;
                 }
             }).filter(e => e);
 
-            // return error code
-            const errorCode = codeGenerator.inputValidation(validator.field, errors[0]);
+            // return the code
             const successCode = {
                 name: 'VALIDATION_PASSED',
                 status: 200,
                 success: true
             };
-            return errors.length ? errorCode : successCode;
-        }).filter(x => x)[0];
+            return errors.length ? codeGenerator.inputValidation(validator.field, errors[0]) : successCode;
+        }).filter(x => x).sort((a,b) => (a.success > b.success) ? 1 : ((b.success > a.success) ? -1 : 0))[0];
+
     }
 
 };
